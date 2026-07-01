03058c326aa0e7bcfa0d53677d0550921dfa34ff
pranay8873 <pranaypuyaati@gmail.com>
2026-07-02 01:56:50 +0530

    removed builder

--- src/main/java/com/spring/mongidb/DTOs/CustomerLogInDTO.java
+++ src/main/java/com/spring/mongidb/DTOs/CustomerLogInDTO.java
@@ -1,10 +1,13 @@
 package com.spring.mongidb.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerLogInDTO {
     private String email;
     private String password;
