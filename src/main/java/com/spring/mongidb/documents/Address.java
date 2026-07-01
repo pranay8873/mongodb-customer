4ff4b1fa84b5d8534f90643f98812c70777ac88e
pranay8873 <pranaypuyaati@gmail.com>
2026-07-02 01:56:14 +0530

    removed @builder bcoz if it present conversion of json is not possible for spring so insted we use @noargs and @allargs constructors

--- src/main/java/com/spring/mongidb/documents/Address.java
+++ src/main/java/com/spring/mongidb/documents/Address.java
@@ -1,10 +1,13 @@
 package com.spring.mongidb.documents;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
     private String doorNo;
     private String street;
--- src/main/java/com/spring/mongidb/documents/Customer.java
+++ src/main/java/com/spring/mongidb/documents/Customer.java
@@ -1,13 +1,16 @@
 package com.spring.mongidb.documents;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "customers")
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
     @Id
     private String id;
